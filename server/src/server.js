24f049eceda0869153782d894fe9e27119bbbea6
Sneakyweasel90 <neil.mannion1@gmail.com>
2026-03-20 08:13:45 -0300

    update server

--- server/src/server.js
+++ server/src/server.js
@@ -35,7 +35,7 @@ app.use(cors({
   origin: (origin, callback) => {
     if (!origin) return callback(null, true);
     if (allowedOrigins.includes(origin)) return callback(null, true);
-    callback(new Error("Not allowed by CORS"));
+    callback(null, false);
   },
   credentials: true,
 }));

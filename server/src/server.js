be7ba6c4fdfc6c439b06e522f09e33c7fa11cac3
Sneakyweasel90 <neil.mannion1@gmail.com>
2026-03-24 14:53:15 -0300

    fixed crash

--- server/src/server.js
+++ server/src/server.js
@@ -15,7 +15,6 @@ import { initWebSocket } from "./websocket/gateway.js";
 import { apiLimiter } from "./middleware/rateLimit.js";
 import voiceRoutes from "./routes/voice.routes.js";
 import helmet from "helmet"
-import settingsRoutes from "./routes/settings.routes.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -45,7 +44,6 @@ app.use(cors({
 app.use(express.json({ limit: "2mb" }));
 app.use("/api", apiLimiter);
 
-app.use("/api/settings", settingsRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/users", usersRoutes);
 app.use("/api/channels", channelsRoutes);
